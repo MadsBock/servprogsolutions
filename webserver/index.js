91344c0bdbc68c101b58d693cbc10d19134d86f7
Mads Bock <bockchristensen@gmail.com>
2021-02-19 08:41:18 +0100

    Added the serverside scripting for webserver

--- webserver/index.js
+++ webserver/index.js
@@ -1,1 +1,46 @@
+const http = require("http")
+const fs = require("fs")
+
+const server = http.createServer((req,res)=>{
+    //Log the request url for debugging purposes
+    console.log(req.url)
+
+    //first, split the parameteres away from the path of the url.
+    //before: /test?foo=bar
+    //after: /test
+    var path = req.url.split("?")[0]
+    
+    //second, add the folder to the path
+    //before: /test
+    //after: ./docs/test
+    path = "docs" + path
+
+    //If the path is a folder, append 'index.html'
+    if(path.endsWith("/")){
+        path = path + "index.html"
+    }
+
+    //use the path to find the file
+    fs.readFile(path, (err, data)=>{
+        if(err) {
+            console.error(err)
+            res.statusCode = 404
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+
+
 console.log("This server serves the files in the /docs folder")
+console.log("Listening on port 8080")
+server.listen(8080)
+
+/*
+Suggestions for better practice:
+ - Use JS Promises instead of callbacks
+ - More precise error handling (i.e. don't just say 404)
+ - Use the "mime-types" package to find the correct mime type and use it to set the Content-Type header
+*/
