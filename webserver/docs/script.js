d8e69f6b12f470a41bb72a2e382dbd467516822a
Mads Bock <bockchristensen@gmail.com>
2021-02-19 08:20:50 +0100

    Made web testing app

---
+++ webserver/docs/script.js
@@ -0,0 +1,3 @@
+function buttonclick() {
+    document.getElementById("testingDiv").innerHTML = "Javascript Works"
+}
---
+++ webserver/index.js
@@ -0,0 +1,1 @@
+console.log("This server serves the files in the /docs folder")
